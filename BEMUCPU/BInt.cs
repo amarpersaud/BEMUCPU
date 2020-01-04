b81bcc9289c52af8097bf88a0dcc154b9340c86e
Amar Persaud <tehspartaa@gmail.com>
2020-01-03 22:49:46 -0500

    Add base BInt class for arbitrarily size words. Simplifies shared code between registers, busses, other indexed sections. Will allow 4/8/16 bit integers as well as odd busses like the wider control logic bus / etc.

---
+++ BEMUCPU/BInt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEMUCPU
+{
+    public class BInt
+    {        
+        /// <summary>
+        /// Underlying values.
+        /// </summary>
+        private bool[] Values;
+
+        public BInt(int size)
+        {
+            Values = new bool[size];
+        }
+
+        /// <summary>
+        /// Size of the register in bits
+        /// </summary>
+        public int Size
+        {
+            get { return Values.Length; }
+        }
+
+        /// <summary>
+        /// Get/set the bit at [index]
+        /// </summary>
+        /// <param name="index">Bit to get/set</param>
+        /// <returns>Bit at [index]</returns>
+        public bool this[int index]
+        {
+            get
+            {
+                return Values[index];
+            }
+            set
+            {
+                Values[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Reset the values This is unused on registers because
+        /// the octal D-Flip flops used don't have a reset pin; just 
+        /// power, ground, 8 inputs, 8 outputs, clock, and output enable
+        /// </summary>
+        private void Reset()
+        {
+            for (int i = 0; i < Values.Length; i++)
+            {
+                Values[i] = false;
+            }
+        }
+    }
+}
--- BEMUCPU/Register.cs
+++ BEMUCPU/Register.cs
@@ -13,13 +13,8 @@ namespace BEMUCPU
     /// 
     /// Positive edge triggered.
     /// </summary>
-    public class Register
+    public class Register : BInt
     {
-        /// <summary>
-        /// Underlying values.
-        /// </summary>
-        private bool[] Values;
-
         /// <summary>
         /// Name of the Register. Not necessary, just useful for
         /// keeping track of things.
@@ -27,12 +22,19 @@ namespace BEMUCPU
         public string Name;
 
         /// <summary>
+        /// Output enabled
+        /// </summary>
+        private bool _oe;
+
+        /// <summary>
         /// Output enabled.
         /// </summary>
         public bool OutputEnable
         {
-            get;
-            set;
+            set
+            {
+                _oe = value;
+            }
         }
 
         /// <summary>
@@ -46,49 +48,10 @@ namespace BEMUCPU
         /// </summary>
         /// <param name="size">Number of bits</param>
         /// <param name="Name">Name of the register</param>
-        public Register(int size, string Name)
+        public Register(int size, string Name) : base(size)
         {
-            Values = new bool[size];
             this.Name = Name;
-
         }
-
-        /// <summary>
-        /// Size of the register in bits
-        /// </summary>
-        public int Size
-        {
-            get { return Values.Length; }
-        }
-
-        /// <summary>
-        /// Get/set the bit at [index]
-        /// </summary>
-        /// <param name="index">Bit to get/set</param>
-        /// <returns>Bit at [index]</returns>
-        public bool this[int index]
-        {
-            get
-            {
-                return Values[index];
-            }
-            set
-            {
-                Values[index] = value;
-            }
-        }
-
-        /// <summary>
-        /// Reset the register. This is private/for debug use because
-        /// the octal D-Flip flops used don't have a reset pin; just 
-        /// power, ground, 8 inputs, 8 outputs, clock, and output enable
-        /// </summary>
-        private void Reset()
-        {
-            for(int i = 0; i < Values.Length; i++)
-            {
-                Values[i] = false;
-            }
-        }
+        
     }
 }
